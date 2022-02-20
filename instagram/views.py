bb6e17c20c3ef73feb6905ce96a2ebfbde2ad6aa
ohdowon064 <dhehdnjs03@gmail.com>
2022-02-20 20:59:02 +0900

    엑셀 응답생성 xlsx

--- instagram/views.py
+++ instagram/views.py
@@ -16,6 +16,7 @@ def post_list(request):
 
 import pandas as pd
 from io import StringIO
+from urllib.parse import quote
 from django.http import HttpResponse
 
 def response_csv(request):
@@ -26,10 +27,11 @@ def response_csv(request):
     ])
 
     io = StringIO
-    df.to_csv(io) # df 내용을 StringIO io 파일에 작성
+    df.to_excel(io)
     io.seek(0) # 끝에 있는 파일 커서를 처음으로 이동
 
-    response = HttpResponse(io, content_type="text/csv")
+    encoded_filename = quote("pandas.xlsx") # quote는 %xx 이스케이프를 사용하여 string의 특수문자를 치환한다.
+    response = HttpResponse(io, content_type="application/vnd.ms-excel")
     response["Content-Disposition"] = "attachment; filename*=utf-8''{}".format(encoded_filename)
     # FileRsponse를 사용하면 header를 자동으로 달아준다.
     return response
